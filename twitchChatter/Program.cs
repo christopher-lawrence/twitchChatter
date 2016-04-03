7ddd27242e9f9be808dd3c640810f94d04bad85e
Christopher Lawrence <christopher.lawrence@citrix.com>
2016-04-03 12:20:08 -0400

    removed settings

--- twitchChatter/Program.cs
+++ twitchChatter/Program.cs
@@ -7,23 +7,25 @@ namespace twitchChatter
 {
     class Program
     {
-        public static string ClientId = "cszuck6wvbeqwwwnviqolurifs8kv2i";
-        public static string ClientSecret = "8nm4gyasr1m54ysq2s3llhd2h6md56k";
-        public static string OAuthToken = "s92tass9c4p1y69t2zokmfvvi7bcd2";
+        public static string ClientId = "CLIENT_ID";
+        public static string ClientSecret = "CLIENT_SECRET";
+        public static string OAuthToken = "OAUTH_TOKEN";
         public static string RedirectUri = "http://localhost";
 
-        public static string TwitchChatOAuthToken = "oauth:qyers1ehicn3si4jd52lk11x8cla3j";
+        public static string TwitchChatOAuthToken = "IRC_OAUTH_TOKEN";
 
         public static string host = "irc.chat.twitch.tv";
         public static int port = 80;
-        public static string username = "derpy_derper";
+        public static string username = "USERNAME";
+
+        public static string channel = "CHANNEL_TO_JOIN";
 
         static void Main(string[] args)
         {
             var client = new TwitchClient(host, port);
             Task.WhenAll(client.Login(username, TwitchChatOAuthToken));
             Thread.Sleep(5);
-            Task.WhenAll(client.Join("thijshs"));
+            Task.WhenAll(client.Join(channel));
 
             var autoReply = new AutoReply(client);
 
